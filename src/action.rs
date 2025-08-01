4e1ffa93d9693e36ca83892adc3cc5932b876a42
Shachar Meir <shachar.meir007@gmail.com>
2025-08-02 00:56:34 +0300

    cli parser improved
    Used the clap crater to create a cli parser that is more powerful and instructive than the previous parser that was written manually.
    The parser can now parse commands and command arguments more easily (and it is easy to add commands and parameters). This allowed to make sure that Add can get all the parameters required for a todo item.

--- src/action.rs
+++ src/action.rs
@@ -1,36 +1,64 @@
 use std::error::Error;
 use std::fs::{self, File};
 use std::io::Write;
+use clap::{Subcommand, Parser};
+use chrono::NaiveDate;
 
 use crate::models::TodoItem;
 use crate::storage::Storage;
 
-pub enum Action<'a> {
-    Add(&'a str),
-    List,
-    Remove(usize),
-    Complete(u32),   
+#[derive(Parser)]
+#[command(name = "todo", version, about = "A simple todo CLI app")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
 }
-impl<'a> Action<'a> {
+
+fn parse_date(s: &str) -> Result<NaiveDate, chrono::ParseError> {
+    NaiveDate::parse_from_str(s, "%Y-%m-%d")
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Add {
+        description: String,
+        #[arg(long="p")]
+        priority: Option<u8>,
+        #[arg(long="due", value_parser=parse_date)]
+        due_date: Option<NaiveDate>,
+    },
+    List,
+    Remove {
+        index: usize,
+    },
+    Complete {
+        index: usize,
+    },
+}
+
+
+impl Commands {
+
+// impl Commands {
     // this function parses command: &str and command_args into the action and its parameters.
-    pub fn new(command: &str, command_args: &'a str) -> Result<Self, String>{
-        match command {
-                "add" => Ok(Action::Add(command_args)),
-                "list" => Ok(Action::List),
-                "remove" => 
-                    command_args.parse().map(Action::Remove).map_err(|e| {format!("Err:{e:?}")})
-                ,
-                "complete" => command_args.parse().map(Action::Complete).map_err(|e| {format!("Err:{e:?}")}),
-                _ => Err(format!("The command {command} is not supported"))
-            }
-    }
+    // pub fn new(command: &str, command_args: &'a str) -> Result<Self, String>{
+    //     match command {
+    //             "add" => Ok(Action::Add(command_args)),
+    //             "list" => Ok(Action::List),
+    //             "remove" => 
+    //                 command_args.parse().map(Action::Remove).map_err(|e| {format!("Err:{e:?}")})
+    //             ,
+    //             "complete" => command_args.parse().map(Action::Complete).map_err(|e| {format!("Err:{e:?}")}),
+    //             _ => Err(format!("The command {command} is not supported"))
+    //         }
+    // }
 
     pub fn execute<T: Storage>(&self, storage: T) -> Result<(), Box<dyn Error>> {
         match self {
-            Action::Add(name) => {  
-                storage.add(TodoItem::new(name.to_string(), None, None))
+            Commands::Add{description, due_date, priority} => {  
+                storage.add(TodoItem::new(description.to_string(), *priority, *due_date))
             },
-            Action::List => {
+            Commands::List => {
                 let list = storage.load()?;
                 for (i, line) in list.iter().enumerate() {
                     println!("Item {}: {line:?}", i+1);
@@ -38,7 +66,7 @@ impl<'a> Action<'a> {
                 Ok(())
                 
             },
-            Action::Remove(index) => {
+            Commands::Remove{index} => {
                 
                 let content = fs::read_to_string("list.txt").unwrap();
                 let mut new_content = String::from("");
@@ -63,10 +91,79 @@ impl<'a> Action<'a> {
                 println!("Removed item number {index}");
                 Ok(())
             },
-            Action::Complete(index) => {
+            Commands::Complete{index} => {
                 println!("Marked item number {index} as completed");
                 Ok(())
             },
         }
     }
 }
+
+// pub enum Action<'a> {
+//     Add(&'a str),
+//     List,1
+//     Remove(usize),
+//     Complete(u32),   
+// }
+// impl<'a> Action<'a> {
+
+// // impl Commands {
+//     // this function parses command: &str and command_args into the action and its parameters.
+//     pub fn new(command: &str, command_args: &'a str) -> Result<Self, String>{
+//         match command {
+//                 "add" => Ok(Action::Add(command_args)),
+//                 "list" => Ok(Action::List),
+//                 "remove" => 
+//                     command_args.parse().map(Action::Remove).map_err(|e| {format!("Err:{e:?}")})
+//                 ,
+//                 "complete" => command_args.parse().map(Action::Complete).map_err(|e| {format!("Err:{e:?}")}),
+//                 _ => Err(format!("The command {command} is not supported"))
+//             }
+//     }
+
+//     pub fn execute<T: Storage>(&self, storage: T) -> Result<(), Box<dyn Error>> {
+//         match self {
+//             Action::Add(name) => {  
+//                 storage.add(TodoItem::new(name.to_string(), None, None))
+//             },
+//             Action::List => {
+//                 let list = storage.load()?;
+//                 for (i, line) in list.iter().enumerate() {
+//                     println!("Item {}: {line:?}", i+1);
+//                 }
+//                 Ok(())
+                
+//             },
+//             Action::Remove(index) => {
+                
+//                 let content = fs::read_to_string("list.txt").unwrap();
+//                 let mut new_content = String::from("");
+//                 if *index == 0 {
+//                     return Err("indeces start at 1".into());
+//                 }
+//                 let count = content.lines().count();
+//                 let lines = if count < *index {
+//                     return Err(format!("There is no item {index}").into());
+//                 } else {
+//                     content.lines()
+//                 };
+//                 for (i, line) in lines.enumerate() {
+//                     if *index != i+1 {
+//                         new_content.push_str(
+//                             if i+1 < count { format!("{line}\n")} else {format!("{line}")}
+//                             .as_str());
+//                     }
+//                 }
+//                 let mut file = File::create("list.txt")?;
+//                 write!(file, "{new_content}")?;
+//                 println!("Removed item number {index}");
+//                 Ok(())
+//             },
+//             Action::Complete(index) => {
+//                 println!("Marked item number {index} as completed");
+//                 Ok(())
+//             },
+//         }
+//     }
+// }
+
